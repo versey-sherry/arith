2ce977a3358f44434d10b8165fef5477d8350159
Sherry <sherrylin42@gmail.com>
2020-01-14 14:41:31 -0800

    starting with lexer

--- arith.py
+++ arith.py
@@ -8,6 +8,7 @@ class Token():
 	#String representation for debugging just in case
 	def __repr__(self):
 		return 'Token({type},{value})'.format(type = self.type, value = repr(self.value))
+#lexer
 class Lexer():
 	def__init__(self, text):
 	self.text = text
@@ -15,8 +16,35 @@ class Lexer():
 	self.pos = 0
 	self.current_char = self.text[self.pos]
 
+	def error(self):
+		raise Exception("Invalid inputs")
+
+	#advance to the next character
+	def next(self):
+		self.pos +=1
+		if self.pos > len(text) -1:
+			self.current_char = None
+		else:
+			self.current_char = self.text[self.pos]
+
+	#skipping white spaces
+	def skipwhitespace(self):
+		while self.current_char is not None and self.current_char.isspace():
+			self.next()
+
+	#multiple digits
+	def
 #Parser
 #Parse the tokens into an AST
 
 #Interpreter
 #Evaluate the programing with AST
+
+def main():
+	while True:
+		try:
+			#Taking raw inputs
+			text = input()
+		except EOFError:
+			break
+
